9198a347d927c7c86a3fb2c7ae2d8f8d0edecbd5
vietean <vietean@dc87401c-29b7-2b42-93b8-b0508b7741ae>
2011-03-30 05:20:52 +0000

    - Update

---
+++ trunk/box2d/Examples/TestBed/Tests/JointTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Box2DX.Dynamics;
+
+namespace TestBed
+{
+    class JointTest : Test
+    {
+        public JointTest()
+        {
+            RevoluteJointDef jointDef = new RevoluteJointDef();
+        }
+    }
+}
