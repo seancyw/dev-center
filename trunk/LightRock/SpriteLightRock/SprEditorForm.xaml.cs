8a9ef87c5fa58915a0c238f57513b08f357aa6b3
vietean <vietean@dc87401c-29b7-2b42-93b8-b0508b7741ae>
2011-03-30 13:17:38 +0000

    - Update

---
+++ trunk/LightRock/SpriteLightRock/SprEditorForm.xaml.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: phu.tranphong
+ * Date: 3/30/2011
+ * Time: 1:24 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace SpriteLightRock
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class SprEditorForm : Window
+	{
+		public SprEditorForm()
+		{
+			InitializeComponent();
+		}        
+	}
+}
