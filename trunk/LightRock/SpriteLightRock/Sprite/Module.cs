d79338f2735700024794e94bf5712fadffeb78f8
vietean <vietean@dc87401c-29b7-2b42-93b8-b0508b7741ae>
2011-03-31 04:33:57 +0000

    + Add Module.cs

---
+++ trunk/LightRock/SpriteLightRock/Sprite/Module.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpriteLightRock.Sprite
+{
+    class Module
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Module(int x, int y, int w, int h)
+        {
+            X = x;
+            Y = y;
+            Width = w;
+            Height = h;
+        }
+    }
+}
