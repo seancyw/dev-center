acab0b588e497b15dddb298bf6fa105fdabf0a1a
vietean <vietean@dc87401c-29b7-2b42-93b8-b0508b7741ae>
2011-04-05 06:42:37 +0000

    - Update

--- trunk/LightRock/SpriteLightRock/Sprites/Frame.cs
+++ trunk/LightRock/SpriteLightRock/Sprites/Frame.cs
@@ -5,49 +5,28 @@ using System.Text;
 
 namespace SpriteLightRock.Sprites
 {
-    class Frame
+    class Frame : SpriteHightObject
     {
-        private int _id;
-        private int _x;
-        private int _y;
-        private int _scaleX;
-        private int _scaleY;
-        private int _angle;
-        private List<FModule> _listFModules;        
-        public int Id
+        private class LocalDef
         {
-            get { return _id; }
-            set { _id = value; }
+            public const int FRE_FIX = 0x0200;
         }
+        private List<FModule> _listFModules;
+
+
+        public override int PreFixId
+        {
+            get
+            {
+                return LocalDef.FRE_FIX;
+            }
+        }
+
+        //
         public List<FModule> FModules
         {
             get { return _listFModules; }
             set { _listFModules = value; }
-        }
-        public int X
-        {
-            get { return _x; }
-            set { _x = value; }
-        }
-        public int Y
-        {
-            get { return _y; }
-            set { _y = value; }
-        }
-        public int ScaleX
-        {
-            get { return _scaleX; }
-            set { _scaleX = value; }
-        }
-        public int ScaleY
-        {
-            get { return _scaleY; }
-            set { _scaleY = value; }
-        }
-        public int Angle
-        {
-            get { return _angle; }
-            set { _angle = value; }
         }
     }
 }
---
+++ trunk/LightRock/SpriteLightRock/Sprites/SpriteLowObject.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace SpriteLightRock.Sprites
+{
+    enum ModuleType : byte
+    {
+        Image,
+        Rectangle,
+        Ellipse,
+        Triangle
+    }  
+    class SpriteLowObject : DependencyObject
+    {
+        #region Id
+        public static readonly DependencyProperty IdProperty = DependencyProperty.Register(
+            "Id",
+            typeof(int),
+            typeof(SpriteLowObject),
+            new UIPropertyMetadata(null)
+        );
+        public int Id
+        {
+            get { return (int)GetValue(IdProperty); }
+            set { SetValue(IdProperty, value); }
+        }
+        #endregion
+
+        #region <bold>X</bold>
+        public static readonly DependencyProperty XProperty = DependencyProperty.Register(
+            "X",
+            typeof(int),
+            typeof(SpriteLowObject),
+            new UIPropertyMetadata(null)
+        );
+        public int X
+        {
+            get { return (int)GetValue(XProperty); }
+            set { SetValue(XProperty, value); }
+        }
+        #endregion
+
+
+        public static readonly DependencyProperty YProperty = DependencyProperty.Register(
+            "Y",
+            typeof(int),
+            typeof(SpriteLowObject),
+            new UIPropertyMetadata(null)
+        );
+        public int Y
+        {
+            get { return (int)GetValue(YProperty); }
+            set { SetValue(YProperty, value); }
+        }
+        public virtual int PreFixId
+        {
+            get { return 0; }
+        }
+    }
+}
