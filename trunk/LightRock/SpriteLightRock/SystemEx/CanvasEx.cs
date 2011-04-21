f83e178374621c7d9771cc829d4663ebf75961d6
vietean <vietean@dc87401c-29b7-2b42-93b8-b0508b7741ae>
2011-04-21 07:29:46 +0000

    - Update - Drawing Image. With Zoom In/Out

--- trunk/LightRock/SpriteLightRock/SystemEx/CanvasEx.cs
+++ trunk/LightRock/SpriteLightRock/SystemEx/CanvasEx.cs
@@ -14,9 +14,29 @@ namespace SpriteLightRock.SystemEx
     using Sprites;
     class CanvasEx : Canvas
     {
-        private Sprite          _currentSprite;        
-        private Point           _currentPoint;
-        
+        static      private Sprite          _currentSprite;
+        static      private MatrixTransform _transform;
+        static private ScaleTransform       _scale = new ScaleTransform();
+        static private TranslateTransform   _translate;
+        private     Point                   _currentPoint;
+        private const double MAX_WIDTH = 10000;
+        private const double MAX_HEIGHT = 10000;
+
+        public Point Offset
+        {
+            get
+            {
+                return new Point(300, 300);
+            }
+        }
+
+        public Point Center
+        {
+            get{
+                return new Point(MAX_WIDTH / 2, MAX_HEIGHT / 2);
+            }
+        }
+
         public Sprite CurrentSprite
         {
             get
@@ -32,6 +52,8 @@ namespace SpriteLightRock.SystemEx
         public CanvasEx()
         	:base()
         {
+            _transform = (MatrixTransform)MatrixTransform.Identity;
+            _translate = new TranslateTransform(-(Center.X - Offset.X), -(Center.Y - Offset.Y));
         }
         public bool IsDrag
         {
@@ -59,11 +81,12 @@ namespace SpriteLightRock.SystemEx
             base.OnRender(dc);
             if (CurrentSprite == null)
                 return;
-            Transform t = Transform.Identity;    
-            dc.PushTransform(new RotateTransform(45,Width/2,Height/2));
-            dc.PushTransform(new ScaleTransform(0.25f,0.25f));
-            dc.PushTransform(new TranslateTransform(200,300));
-            dc.DrawImage(CurrentSprite.Bitmap, new Rect(30,20,CurrentSprite.Bitmap.Width,CurrentSprite.Bitmap.Height));
+            //_transform.Value.Rotate(45);
+            dc.PushTransform(_scale);            
+            dc.PushTransform(_translate);
+            dc.DrawLine(new Pen(Brushes.Red, 1),new Point(Center.X,0),new Point(Center.X,MAX_HEIGHT));
+            dc.DrawLine(new Pen(Brushes.Green, 1), new Point(0, Center.Y), new Point(MAX_WIDTH, Center.Y));
+            dc.DrawImage(CurrentSprite.Bitmap, new Rect(Center.X,Center.Y,CurrentSprite.Bitmap.Width,CurrentSprite.Bitmap.Height));
         }
         protected override void OnMouseDown(MouseButtonEventArgs e)
         {
@@ -78,6 +101,30 @@ namespace SpriteLightRock.SystemEx
         {
             base.OnMouseMove(e);
             PointCurrent = e.GetPosition(this);
+        }
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                _scale.ScaleX += 0.1;
+                _scale.ScaleY += 0.1;
+            }
+            else
+            {
+                _scale.ScaleX -= 0.1;
+                _scale.ScaleY -= 0.1;
+            }
+            base.OnMouseWheel(e);
+        }
+        //Hien tai cai canvas khong the nhan dc su kien bam phim
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if (e.Key == Key.NumPad1)
+            {
+                _scale.ScaleX = 1;
+                _scale.ScaleY = 1;
+            }
+            base.OnKeyDown(e);
         }
         protected override void OnMouseUp(MouseButtonEventArgs e)
         {
